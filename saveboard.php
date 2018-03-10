f9f5670f8ea94387c09fedbc3c1e7541614b3613
Unai <unai.san.rod@techtalents.club>
2018-03-10 12:53:19 +0100

    Add files via upload

---
+++ saveboard.php
@@ -0,0 +1,16 @@
+<?php
+$serverName  = "localhost";
+$username = "unaisanrod";
+include 'password.php';
+$dbName = 'unaisanrod';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Board saved</title>
+</head>
+<body>
+    
+</body>
+</html>
